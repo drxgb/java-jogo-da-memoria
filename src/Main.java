d2492ae3962359214552c73fd8b6c137c9be6c35
unknown <dr.xgb.rm2k@gmail.com>
2020-04-26 11:39:17 -0300

    Projeto criado

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * JOGO DA MEMÓRIA
+ * 
+ * @author Dr.XGB
+ * @version 1.0
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo! o/");
+
+	}
+
+}
